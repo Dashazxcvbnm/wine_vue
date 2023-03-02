3b259f2bc7b520414c7c5f9ba555be53e1b3fa07
Dasha <dashalis1402@gmail.com>
2023-03-02 20:08:53 +0300

    Начало работы над второй секцией

---
+++ src/components/BorderContentConteiner.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="border-wrapper">
+        <h1 class="section-title">{{ title }}</h1>
+        <span>{{ text }}</span>
+       <slot> <span></span></slot>
+    </div>
+</template>
+
+<script setup>
+
+defineProps(['title', 'line', 'text'])
+
+</script>
+
+<style>
+
+.border-wrapper {
+    border: 3px solid rgb(196, 196, 196);
+    min-height: 600px;
+    margin-top: 100px;
+    margin-bottom: 100px;
+}
+
+.section-title {
+    font-family: 'Playfair Display';
+    font-size: 30px;
+    color: rgb(255, 255, 255);
+}
+</style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -12,6 +12,29 @@
         </div>
 
     </ContentWrapper>
+
+    <ContentWrapper class="section--second">
+        <BorderContentConteiner
+        title="Лучший вкус, это вкус долголетнего вина"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Feugiat enim tortor in hac id imperdiet adipiscing. 
+        Pellentesque nisi, mi sit non sit sed fermentum. 
+        Felis adipiscing morbi sodales ac. 
+        Mauris dictumst risus pulvinar blandit elit. 
+        Vestibulum quam ultrices nascetur et viverra suscipit. 
+        Proin vitae aliquet leo aliquam amet rutrum. 
+        Lectus auctor purus ultrices enim ultrices. 
+        Augue fringilla tellus tortor orci ultrices sed. 
+        Viverra cras sapien, pellentesque viverra malesuada. 
+        Tellus dolor, eget vitae dignissim molestie eget sit duis. 
+        Vitae dui vel pretium euismod diam. 
+        Pellentesque amet, lacus, amet, quis risus. 
+        Pellentesque scelerisque nunc, orci aliquam quis."
+        class="section--second-title">
+        <span>'isdnvecioun'</span>
+        </BorderContentConteiner>
+    </ContentWrapper>
+
  </div>
 </template>
 
@@ -21,6 +44,7 @@ import ContentWrapper from '../components/ContentWrapper.vue';
 import PageHeader from '../components/GUI/PageHeader.vue';
 import MenuItem from '../components/GUI/MenuItem.vue';
 import ButtonShape from '../components/GUI/ButtonShape.vue';
+import BorderContentConteiner from '../components/BorderContentConteiner.vue';
 
 import { ref } from 'vue'
 
@@ -52,6 +76,16 @@ const items = ref([
 .control-button {
     margin: 15px;
 }
+
+.section--second {
+    background: rgb(40, 40, 40);
+    min-height: 856px;
+    display: flex;
+}
+
+.section--second-title {
+}
+
 </style>
 
 
