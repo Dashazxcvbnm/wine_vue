991c91d58cc1390ddd643d6d9a9297f1bcf2668a
Dasha <dashalis1402@gmail.com>
2023-03-03 18:36:54 +0300

    Вторая секция разбита на переипользуемые компоненты, применены стили

---
+++ src/components/GUI/WineVueText.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="section-text">{{ text }}</div>
+    </div>
+</template>
+
+<script setup>
+
+defineProps({
+    text: String
+})
+
+</script>
+
+<style scoped>
+
+.section-text {
+    font-family: 'Montserrat';
+    line-height: 24px;
+    text-align: center;
+}
+</style>
---
+++ src/components/GUI/WineVueTitle.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1 class="section-title">{{ text }}</h1>
+</template>
+
+<script setup>
+
+defineProps({
+    text: String
+})
+
+</script>
+
+<style scoped>
+
+.section-title {
+    font-family: 'Playfair Display';
+    font-size: 30px;
+    color: rgb(255, 255, 255);
+    text-align: center;
+    line-height: 54px;
+}
+</style>
