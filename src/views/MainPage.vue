9c5cf2f81ac35b828c4aff0282e01be5bbce0514
Dasha <dashalis1402@gmail.com>
2023-03-03 19:58:09 +0300

    Исправленны стили

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,10 +1,12 @@
 <template>
 <NavigationSection/>
 <SloganSection/>
+<NoveltySection/>
 </template>
 
 <script setup>
 import NavigationSection from '../components/NavigationSection.vue';
 import SloganSection from '../components/SloganSection.vue';
+import NoveltySection from '../components/NoveltySection.vue';
 
 </script>
