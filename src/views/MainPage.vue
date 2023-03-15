72f55791608f8b0d6762ceb53e2ec3fde8c7aa3c
Dasha <dashalis1402@gmail.com>
2023-03-15 13:37:10 +0300

    Создан футер страницы, применены стили

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -3,6 +3,7 @@
 <SloganSection/>
 <NoveltySection/>
 <RegistrationSection/>
+<FooterHeader/>
 
 </template>
 
@@ -11,5 +12,6 @@ import NavigationSection from '../components/NavigationSection.vue';
 import SloganSection from '../components/SloganSection.vue';
 import NoveltySection from '../components/NoveltySection.vue';
 import RegistrationSection from '../components/RegistrationSection.vue';
+import FooterHeader from '../components/FooterHeader.vue';
 
 </script>
